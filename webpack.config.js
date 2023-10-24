e11228265768eb86c73166c06bac67e02b18887d
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-24 13:54:17 +0300

    created event

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => {
   const config = {
     entry: './src/index.jsx',
     output: {
-      filename: 'build.js',
+      filename: 'bundle.js',
     },
     module: {
       rules: [
