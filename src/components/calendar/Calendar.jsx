fb4a7c183697a41ca06948d162b1c225b7571995
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-10-30 23:40:18 +0200

    created event

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navigation from '../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
@@ -6,6 +6,8 @@ import events from '../../gateway/events';
 import './calendar.scss';
 
 function Calendar(props) {
+  const [eventState, setEventState] = useState(events);
+
   return (
     <section className="calendar">
       <Navigation weekDates={props.weekDates} />
