bb0702df2e428919710cab7ac3b516288d1a6068
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-11-03 16:07:12 +0200

    created event

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,12 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navigation from '../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
-import events from '../../gateway/events';
+import {
+  fetchEvents,
+  onCreateTask,
+  onDeleteTask,
+} from '..//..//gateway/events';
 import './calendar.scss';
 
 function Calendar(props) {
-  const [eventState, setEventState] = useState(events);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Выполнение GET-запроса для получения событий с сервера MockAPI
+    fetchEvents()
+      .then((data) => {
+        setEvents(data); // Установка полученных событий в состояние
+      })
+      .catch((error) => {
+        console.error('Ошибка при получении событий:', error);
+      });
+  }, []);
+
+  const handleCreateEvent = (eventData) => {
+    onCreateTask(eventData)
+      .then(() => {
+        // Обработка успешного создания события, если необходимо
+      })
+      .catch((error) => {
+        console.error('Ошибка при создании события:', error);
+      });
+  };
+
+  const handleDeleteEvent = (eventId) => {
+    onDeleteTask(eventId)
+      .then(() => {
+        // Обработка успешного удаления события, если необходимо
+      })
+      .catch((error) => {
+        console.error('Ошибка при удалении события:', error);
+      });
+  };
 
   return (
     <section className="calendar">
@@ -14,7 +49,12 @@ function Calendar(props) {
       <div className="calendar__body">
         <div className="calendar__week-container">
           <Sidebar />
-          <Week weekDates={props.weekDates} events={props.events} />
+          <Week
+            weekDates={props.weekDates}
+            events={props.events}
+            onCreateTask={handleCreateEvent}
+            onDeleteTask={handleDeleteEvent}
+          />
         </div>
       </div>
     </section>
--- src/gateway/events.js
+++ src/gateway/events.js
@@ -1,19 +1,66 @@
-const events = [
-  {
-    id: 1,
-    title: 'breakfast',
-    description: 'with mom',
-    dateFrom: new Date(2023, 9, 28, 8, 30),
-    dateTo: new Date(2023, 9, 28, 10, 30),
-  },
+const baseUrl = 'https://654387a401b5e279de2088a9.mockapi.io/api/v1/events';
 
-  {
-    id: 2,
-    title: 'haircurt',
-    description: '',
-    dateFrom: new Date(2023, 9, 30, 10, 30),
-    dateTo: new Date(2023, 9, 30, 12, 0),
-  },
-];
+export const fetchEvents = () =>
+  fetch(baseUrl)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Network error');
+      }
+      return res.json();
+    })
+    .catch(() => {
+      alert("Network Error! Can't display events.");
+      return [];
+    });
 
-export default events;
+export const onCreateTask = (taskInfo) =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(taskInfo),
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Network error');
+      }
+    })
+    .catch(() => {
+      alert("Network Error! Can't create event.");
+      return;
+    });
+
+export const onDeleteTask = (id) =>
+  fetch(`${baseUrl}/${id}`, {
+    method: 'DELETE',
+  })
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Network error');
+      }
+    })
+    .catch(() => {
+      alert("Network Error! Can't delete event.");
+      return;
+    });
+
+// const events = [
+//   {
+//     id: 1,
+//     title: 'breakfast',
+//     description: 'with mom',
+//     dateFrom: new Date(2023, 9, 28, 8, 30),
+//     dateTo: new Date(2023, 9, 28, 10, 30),
+//   },
+
+//   {
+//     id: 2,
+//     title: 'haircurt',
+//     description: '',
+//     dateFrom: new Date(2023, 9, 30, 10, 30),
+//     dateTo: new Date(2023, 9, 30, 12, 0),
+//   },
+// ];
+
+// export default events;
