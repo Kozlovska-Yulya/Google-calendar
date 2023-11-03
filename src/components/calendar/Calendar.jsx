9ec14a06bfd40cbde2cac05b1910d540dbeeea77
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-11-03 16:39:03 +0200

    created event

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -2,46 +2,21 @@ import React, { useState, useEffect } from 'react';
 import Navigation from '../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
-import {
-  fetchEvents,
-  onCreateTask,
-  onDeleteTask,
-} from '..//..//gateway/events';
+import { fetchEvents } from '..//..//gateway/events';
 import './calendar.scss';
 
 function Calendar(props) {
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    // Выполнение GET-запроса для получения событий с сервера MockAPI
     fetchEvents()
       .then((data) => {
-        setEvents(data); // Установка полученных событий в состояние
+        setEvents(data);
       })
       .catch((error) => {
         console.error('Ошибка при получении событий:', error);
       });
   }, []);
-
-  const handleCreateEvent = (eventData) => {
-    onCreateTask(eventData)
-      .then(() => {
-        // Обработка успешного создания события, если необходимо
-      })
-      .catch((error) => {
-        console.error('Ошибка при создании события:', error);
-      });
-  };
-
-  const handleDeleteEvent = (eventId) => {
-    onDeleteTask(eventId)
-      .then(() => {
-        // Обработка успешного удаления события, если необходимо
-      })
-      .catch((error) => {
-        console.error('Ошибка при удалении события:', error);
-      });
-  };
 
   return (
     <section className="calendar">
@@ -49,12 +24,7 @@ function Calendar(props) {
       <div className="calendar__body">
         <div className="calendar__week-container">
           <Sidebar />
-          <Week
-            weekDates={props.weekDates}
-            events={props.events}
-            onCreateTask={handleCreateEvent}
-            onDeleteTask={handleDeleteEvent}
-          />
+          <Week weekDates={props.weekDates} events={props.events} />
         </div>
       </div>
     </section>
